8c2fdf4e0f4fbba8ae0fd76560034a05e4845237
Isak Karlsson <ik222ne@student.lnu.se>
2023-03-24 22:46:05 +0100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Isak Carlsson</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
